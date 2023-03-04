e6ed4d387d81d639d2f88c2a45091f11870024d8
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2023-03-03 20:49:52 -0800

    Adding args

--- KettleArchivist/__main__.py
+++ KettleArchivist/__main__.py
@@ -2,7 +2,16 @@
 from . import db
 from . import scraper
 from rich.progress import track
+import argparse
 from . import *
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--create-db', action='store_true', default=False, help='Create/Recreate DB')
+    parser.add_argument('-u', '--update-db', action='store_true', default=False, help='Update DB')
+    parser.add_argument('-d', '--download', action='store_true', default=False, help='Download Videos (Resume from first non saved entry)')
+    parser.add_argument('-r', '--redownload', action='store_true', default=False, help='Redownload all videos')
+    return parser.parse_args()
 
 def load_videos_in_db():
     videos = scraper.get_videos()
